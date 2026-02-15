f52392f96cb0caa2fb6f3ed03cc81cfe212b356e
Pablo Herrera <pabloherrerapalacio@gmail.com>
2026-02-15 02:11:29 +0100

    Implement Trims module (#1623)
    Signed-off-by: Pablo Herrera <pabloherrerapalacio@gmail.com>

--- platform/platform-modern/src/main/java/tc/oc/pgm/platform/modern/modules/ModernModuleRegistrar.java
+++ platform/platform-modern/src/main/java/tc/oc/pgm/platform/modern/modules/ModernModuleRegistrar.java
@@ -3,6 +3,9 @@ package tc.oc.pgm.platform.modern.modules;
 import static tc.oc.pgm.util.platform.Supports.Variant.PAPER;
 
 import tc.oc.pgm.api.Modules;
+import tc.oc.pgm.platform.modern.modules.trim.TrimMatchModule;
+import tc.oc.pgm.platform.modern.modules.trim.TrimModule;
+import tc.oc.pgm.platform.modern.modules.waypoint.WaypointMatchModule;
 import tc.oc.pgm.platform.modern.tracker.ModernTrackerMatchModule;
 import tc.oc.pgm.util.platform.Supports;
 
@@ -16,5 +19,6 @@ public class ModernModuleRegistrar implements Modules.ModuleRegistrar {
     modules.register(WaypointMatchModule.class, WaypointMatchModule::new);
     modules.register(QuakeModule.class, QuakeMatchModule.class, new QuakeModule.Factory());
     modules.register(ModernTrackerMatchModule.class, ModernTrackerMatchModule::new);
+    modules.register(TrimModule.class, TrimMatchModule.class, new TrimModule.Factory());
   }
 }
